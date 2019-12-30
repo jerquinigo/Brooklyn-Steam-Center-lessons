f6b504a5ee3295ffc6b11404c7fa9fef759d815b
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-12-30 15:54:45 -0500

    answers for activity

---
+++ classActivities/dataTypeActivity/dataTypeNumberActivity/numberActivityAnswers.py
@@ -0,0 +1,48 @@
+# only print out whole numbers for this float value
+
+floatNumber = 203.987
+
+print(int(floatNumber))
+
+# round to the nearest whole numbers
+
+roundNumber = 24.6
+print(round(roundNumber))
+
+# add these two values to get the answer 200
+
+firstValue = 100
+thirdValue = "49"
+print(firstValue + 20 + int(thirdValue) + int("30") + 1)
+
+
+# concatinating two strings. Please connect these these strings together to make a sentance.
+
+greetings = "hello i am a"
+typeOf = "Steam student"
+
+print(greetings + " " +  typeOf + "," + " "+ "nice to meet you")
+
+
+#please use import math for these next questions
+import math
+
+# use a math function that returns the current whole number without decimal value.
+#ex. 34.7 will return 34
+
+
+nums = 45.6
+print(math.floor(nums))
+
+# use a math function that returns the next whole number without decimal value.
+# ex 34.7 will return 35
+
+nums2 = 45.6
+print(math.ceil(nums2))
+
+
+#please print boolean true and false on screen
+
+#print statement below
+print(True)
+print(False)
