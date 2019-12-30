98153078911e154cefc5875af544383a49855d72
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-12-29 21:29:12 -0500

    added notes and class activities

---
+++ classActivities/dataTypeActivity/dataTypeStringActivity/stringActivity.py
@@ -0,0 +1,14 @@
+# please correct these errors by using proper syntax to get quotes and apostrophies to work along with any other string syntax errors present
+
+print("Hello Michael, that is Alex's math textbook, he asked me to "make sure no one takes it off his desk")
+
+
+
+    print("Alex's textbook hasn't been removed from the desk. Michael quotes Alex's plan for the day, "got so many subjects to study for", and "I am so swamped)
+
+print "My father is on his way to pick up from school," replied Michael
+ over the phone to Alex, "please hurry up and come back, I haven't got all day"")
+
+
+print("Alex didn't take too long to make it back. "Thanks man, you're awesome!" 
+Michael sighed in relief." 
