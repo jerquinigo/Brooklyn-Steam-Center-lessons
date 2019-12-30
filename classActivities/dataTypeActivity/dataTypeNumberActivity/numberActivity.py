2384bac5ea2286cca4ec87c573684352edae16a7
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-12-30 02:57:18 -0500

    updating notes

---
+++ classActivities/dataTypeActivity/dataTypeNumberActivity/numberActivity.py
@@ -0,0 +1,24 @@
+# only print out whole numbers for this float value
+
+floatNumber = 203.987
+
+# round to the nearest whole numbers
+
+roundNumber = 24.6
+
+# add these two values to get the answer 200
+
+firstValue = 100
+thirdValue = "49"
+print(firstValue + 20 + thirdValue + "30" + 1)
+
+
+# concatinating two strings. Please connect these these strings together to make a sentance.
+
+greetings = "hello i am a"
+typeOf = "Steam student"
+
+print("nice to meet you")
+
+
+#please use import math for these next questions
